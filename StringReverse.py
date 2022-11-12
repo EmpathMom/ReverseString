1d9c2499d5bdf2f07d873d0da34f4d939f2fa70a
EmpathMom <gaylenedavison87@gmail.com>
2022-11-11 23:06:17 -0800

    Reverse string

---
+++ StringReverse.py
@@ -0,0 +1,25 @@
+## polymorphism went over data types in more depth 
+#class, objection, abstraction,encapsulation,inheritance, polymorphism
+# Author :  Gaylene
+# Created by :  Gaylene
+# Architect(s):  Gaylene 
+# Developer(s): Gaylene
+# Created Date: 11/11/22 
+# Description : reverse a string  
+# Version: 1.0  
+# Modified by:Gaylene
+# Modified Date: 11/11/22
+#
+
+
+
+
+
+
+n = input("Enter gnirts here:")#create input by user
+
+print (n[::-1]) #use  print to slice a string
+
+
+
+
